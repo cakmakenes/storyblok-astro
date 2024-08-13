f8a08db7b41b104b0689b8a3b14ea3883581259c
cakmakenes <enescakmak90@gmail.com>
2024-08-13 16:48:00 +0300

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+});
